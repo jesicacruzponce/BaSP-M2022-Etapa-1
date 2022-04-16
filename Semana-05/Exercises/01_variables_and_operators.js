9030066a02d16e1d14476430cb20e1fd6a344180
jesica cruz ponce <jesica.cruz.ponce@gmail.com>
2022-04-15 23:42:27 -0300

    BaSP-W05-2022: block 1: variables and operators completed

---
+++ Semana-05/Exercises/01_variables_and_operators.js
@@ -0,0 +1,23 @@
+//1. Variables y Operadores
+
+console.log('Suma de dos variables numéricas');
+
+var a=10;
+var b=7;
+var c= a+b;
+
+console.log('La suma de A más B es igual a C, o sea que C es igual a '+ c);
+
+console.log('Concatenación dos variables de tipo string');
+
+var nombre='Jesica';
+var apellido= 'Cruz Ponce';
+var nombreCompleto= nombre + apellido;
+console.log('Mi nombre es completo es ' + nombreCompleto);
+
+console.log('Suma del largo de dos variables');
+
+var nombre1='Juan';
+var apellido1='Perez';
+var nombreCompleto1= nombre1 + apellido1;
+console.log('La cantidad de caracteres del nombre completo de Juan es igual a '+ nombreCompleto1.length)
